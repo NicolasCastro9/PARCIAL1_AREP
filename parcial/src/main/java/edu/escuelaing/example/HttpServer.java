9c84353fc41236e80018ad797d7591a62b058297
unknown <nicolas.castro-j@labinfo.is.escuelaing.edu.co>
2024-02-22 15:49:37 -0500

    parcial falta

---
+++ parcial/src/main/java/edu/escuelaing/example/HttpServer.java
@@ -0,0 +1,143 @@
+package edu.escuelaing.example;
+import java.net.*;
+import java.io.*;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class HttpServer {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        ServerSocket serverSocket = null;
+        try {
+            serverSocket = new ServerSocket(36000);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: 35000.");
+            System.exit(1);
+        }
+
+        
+        boolean running = true;
+        Socket clientSocket = null;
+        while (running) {
+            try {
+                System.out.println("Listo para recibir ...");
+                clientSocket = serverSocket.accept();
+            } catch (IOException e) {
+                System.err.println("Accept failed.");
+                System.exit(1);
+            }
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+            String URI = null;
+            boolean fline = true;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println("Received: " + inputLine);
+                if (fline) {
+                    URI = inputLine.split(" ")[1];
+                    System.out.println(URI);
+                    fline = false;
+                }
+                if (!in.ready()) {
+                    break;
+                }
+            }
+            if (URI.startsWith("/consulta")) {
+                outputLine = Response(URI);
+            } else if (URI.startsWith("/cliente")) {
+                outputLine = index();
+            } else {
+                outputLine = error();
+            }
+            out.println(outputLine);
+            out.close();
+            in.close();
+            clientSocket.close();
+        }
+        
+        serverSocket.close();
+    }
+
+    public static String Response(String uriS) throws ClassNotFoundException {
+        String command = uriS.split("=")[1];
+        String output = getCommand(command);
+        return "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + output;
+    }
+
+    public static String getCommand(String command) throws ClassNotFoundException {
+        String output;
+
+        if (command.startsWith("Class")) {
+            output = classCommand(command);
+        } else if (command.startsWith("invoke")) {
+            output = invokeCommand(command);
+        } else if (command.startsWith("unaryInvoke")) {
+            output = unaryCommand(command);
+        } else if (command.startsWith("binaryInvoke")) {
+            output = binaryInvokeCommand(command);
+        } else {
+            output = error();
+        }
+        return output;
+    }
+
+    public static String classCommand(String command) throws ClassNotFoundException {
+        String salida = "";
+        System.out.println("Comando solicitado: " + command);
+        String value = command.split("\\(")[1];
+        value = value.split("\\)")[0];
+        System.out.println("Valor: " + value);
+        Class c = Class.forName(value);
+        Method[] methods = c.getDeclaredMethods();
+        Field[] fields = c.getDeclaredFields();
+        for(Method method : methods){
+           salida += method.toString() + "\r\n";
+        }
+        for(Field field : fields){
+           salida += field.toString() + "\r\n";
+        }
+        return salida;
+    }
+
+    public static String invokeCommand(String command) {
+        String salida = "";
+        String value = command.substring(6, command.length() - 1);
+        String className = value.split(",")[0];
+        String method = value.split(",")[1];
+        Method methods = null;
+
+        try {
+            try {
+                methods = Class.forName(className).getDeclaredMethod(method);
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            }
+        } catch (SecurityException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        String output = "El metodo: " + method + " " + methods;
+        System.out.println(className);
+        return output;
+    }
+
+    public static String unaryCommand(String command) {
+        return "me falta";
+    }
+
+    public static String binaryInvokeCommand(String command) {
+        return "me falta";
+    }
+
+    public static String index() {
+        String output = Respond.index();
+        return output;
+    }
+
+    public static String error(){
+        String output = Respond.error();
+        return output;
+    }
+
+}
---
+++ parcial/src/main/java/edu/escuelaing/example/Respond.java
@@ -0,0 +1,69 @@
+package edu.escuelaing.example;
+
+public class Respond {
+    public static String index() {
+        String output = "HTTP/1.1 200 OK\r\n"
+                + "Content-Type: text/html\r\n"
+                + "\r\n"
+                + "<!DOCTYPE html>\n"
+                + "<html>\n"
+                + "    <head>\n"
+                + "        <title>Form Example</title>\n"
+                + "        <meta charset=\"UTF-8\">\n"
+                + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                + "    </head>\n"
+                + "    <body>\n"
+                + "        <h1>Form with GET</h1>\n"
+                + "        <form action=\"/consulta\">\n"
+                + "            <label for=\"name\">Name:</label><br>\n"
+                + "            <input type=\"text\" id=\"name\" name=\"comando\" value=\"John\"><br><br>\n"
+                + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg()\">\n"
+                + "        </form> \n"
+                + "        <div id=\"getrespmsg\"></div>\n"
+                + "\n"
+                + "        <script>\n"
+                + "            function loadGetMsg() {\n"
+                + "                let nameVar = document.getElementById(\"name\").value;\n"
+                + "                const xhttp = new XMLHttpRequest();\n"
+                + "                xhttp.onload = function() {\n"
+                + "                    document.getElementById(\"getrespmsg\").innerHTML =\n"
+                + "                    this.responseText;\n"
+                + "                }\n"
+                + "                xhttp.open(\"GET\", \"/consulta?comando=\"+nameVar);\n"
+                + "                xhttp.send();\n"
+                + "            }\n"
+                + "        </script>\n"
+                + "\n"
+                + "        <h1>Form with POST</h1>\n"
+                + "        <form action=\"/hellopost\">\n"
+                + "            <label for=\"name\">Name:</label><br>\n"
+                + "            <input type=\"text\" id=\"name\" name=\"comando\" value=\"John\"><br><br>\n"
+                + "            <input type=\"button\" value=\"Submit\" onclick=\"loadGetMsg(postname)\">\n"
+                + "        </form> \n"
+                + "        <div id=\"postrespmsg\"></div>\n"
+                + "\n"
+                + "        <script>\n"
+                + "        </script>\n"
+                + "\n"
+                + "    </body>\n"
+                + "</html>";
+        return output;
+    }
+
+    public static String error(){
+        String output = "HTTP/1.1 200 OK\r\n"
+        + "Content-Type: text/html\r\n"
+        + "\r\n"
+        + "<!DOCTYPE html>\n"
+        + "<html>\n"
+        + "    <head>\n"
+        + "        <meta charset=\"UTF-8\">\n"
+        + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+        + "    </head>\n"
+        + "    <body>\n"
+        + "        <h1>ERROR</h1>\n"
+        + "    </body>\n"
+        + "</html>";
+    return output;
+    }
+}
